d1225a3dccef06ee2d34aa61724bfc32657e192c
Andreyglass1989 <1989andreyglass@gmail.com>
2018-12-15 20:44:27 +0200

    12.Passing Data from Parent to Child Component

--- avia/src/components/components.module.ts
+++ avia/src/components/components.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { StatusComponent } from './status/status';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { StatusDetailComponent } from './status-detail/status-detail';
 
 @NgModule({
-	declarations: [StatusComponent],
+	declarations: [StatusComponent,
+    StatusDetailComponent],
 	imports: [
 		BrowserModule,
 	    IonicModule,
 
 	],
-	exports: [StatusComponent],
+	exports: [StatusComponent,
+    StatusDetailComponent],
 	
 })
 export class ComponentsModule {}
---
+++ avia/src/components/status-detail/status-detail.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+
+/**
+ * Generated class for the StatusDetailComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'status-detail',
+  templateUrl: 'status-detail.html'
+})
+export class StatusDetailComponent {
+  @Input() myObjectNameAny: any;
+  text: string;
+
+  constructor() {
+    console.log('Hello StatusDetailComponent Component');
+    this.text = 'Hello World yahayahha';
+  }
+
+}
