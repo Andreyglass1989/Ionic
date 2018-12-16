5167d68541ffb012e516d21444323467499b3351
Andreyglass1989 <1989andreyglass@gmail.com>
2018-12-16 12:34:11 +0200

    22.Ionic Storage over Cookies

--- avia/src/app/app.component.ts
+++ avia/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { Storage } from '@ionic/storage';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
@@ -18,7 +18,12 @@ export class MyApp {
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(
+    public platform: Platform, 
+    public statusBar: StatusBar, 
+    public splashScreen: SplashScreen,
+    private storage: Storage
+    ) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -26,7 +31,7 @@ export class MyApp {
       // { title: 'Feed', component: StatusComponent },
       { title: 'Home', component: HomePage },
       { title: 'List', component: ListPage },
-      { title: 'Login', component: LoginPage }
+      // { title: 'Login', component: LoginPage }
     ];
 
   }
@@ -35,6 +40,15 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      this.storage.get('authToken').then((val)=>{
+        console.log("You auth token value is", val);
+        if (!val){
+          this.rootPage = LoginPage
+        }
+        // } else {
+        //   this.rootPage = 
+        // }
+      });
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
--- avia/src/app/app.module.ts
+++ avia/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+import { IonicStorageModule } from '@ionic/storage';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
@@ -25,6 +27,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     BrowserModule,
     ComponentsModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
